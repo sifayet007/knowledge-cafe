7e92ee392b60cd760e56d554f67db81af632667c
sifayet007 <sifayet2004@gmail.comm>
2025-04-28 12:31:59 +0600

    added Bookmarks file and blog file

---
+++ src/components/Blog/Blog.jsx
@@ -0,0 +1,8 @@
+import PropTypes from "prop-types";
+const Blog = ({ blog }) => {
+  console.log(blog);
+
+  return <div></div>;
+};
+
+export default Blog;
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -8,7 +8,11 @@ const Blogs = () => {
       .then((res) => res.json())
       .then((data) => setBlogs(data));
   }, []);
-  return <div></div>;
+  return (
+    <div className="md:w-2/3">
+      <h1 className="text-4xl">Blog: {blogs.length}</h1>
+    </div>
+  );
 };
 
 export default Blogs;

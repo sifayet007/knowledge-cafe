5a6005dd5d330b8a2e16f5c7008ca1ab262be0b9
sifayet007 <sifayet2004@gmail.comm>
2025-04-28 13:15:23 +0600

    added auther, title, reading time, posted date, hashtags

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,8 +1,48 @@
 import PropTypes from "prop-types";
-const Blog = ({ blog }) => {
-  console.log(blog);
 
-  return <div></div>;
+const Blog = ({ blog }) => {
+  const {
+    title,
+    cover,
+    author,
+    author_img,
+    reading_time,
+    posted_date,
+    hashtags,
+  } = blog;
+
+  return (
+    <div>
+      <img
+        className="rounded-lg w-full"
+        src={cover}
+        alt={`Cover picture of the title ${title}`}
+      />
+      <div className="flex justify-between items-center">
+        <div className="flex items-center gap-6">
+          <img className="w-[60px]  rounded-full" src={author_img} alt="" />
+          <div>
+            <h3 className="text-2xl font-bold">{author}</h3>
+            <p>{posted_date}</p>
+          </div>
+        </div>
+        <div>
+          <span>{reading_time} min red</span>
+        </div>
+      </div>
+      <h2 className="text-4xl">Title: {title}</h2>
+      <p>
+        {hashtags.map((hash, idx) => (
+          <span key={idx}>
+            <a href="">{hash}</a>
+          </span>
+        ))}
+      </p>
+    </div>
+  );
+};
+Blog.PropTypes = {
+  blog: PropTypes.object.isRequired,
 };
 
 export default Blog;
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Blog from "../Blog/Blog";
 
 const Blogs = () => {
   const [blogs, setBlogs] = useState([]);
@@ -11,6 +12,9 @@ const Blogs = () => {
   return (
     <div className="md:w-2/3">
       <h1 className="text-4xl">Blog: {blogs.length}</h1>
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog}></Blog>
+      ))}
     </div>
   );
 };

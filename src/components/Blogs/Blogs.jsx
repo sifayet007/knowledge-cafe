a92eaa6002c4226f7b68678f20ae800fffa451a6
sifayet007 <sifayet2004@gmail.comm>
2025-04-28 11:38:57 +0600

    first commit

---
+++ src/components/Blogs/Blogs.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+
+const Blogs = () => {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    fetch("feckData.json")
+      .then((res) => res.json())
+      .then((data) => setBlogs(data));
+  }, []);
+  return <div></div>;
+};
+
+export default Blogs;
